b7cc4b71fbaf61658dd64c703ae7f0061dbe093c
ivan096096 <wanwanich@yandex.ru>
2022-01-16 17:48:17 +0300

    lesson_2

---
+++ src/main/java/ru/geekbrains/lessons/lesson_2_Exeption/MyArraySizeException.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lessons.lesson_2_Exeption;
+
+public class MyArraySizeException extends RuntimeException{
+    public MyArraySizeException() {
+
+    }
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+//IndexOutOfBoundsException
