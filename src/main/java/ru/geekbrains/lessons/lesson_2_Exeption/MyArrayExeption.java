8596eb083e73daa75cee4a61cd08cce456f1dcbd
ivan096096 <wanwanich@yandex.ru>
2022-01-22 17:54:58 +0300

    lesson_3

--- src/main/java/ru/geekbrains/lessons/lesson_2_Exeption/MyArrayExeption.java
+++ src/main/java/ru/geekbrains/lessons/lesson_2_Exeption/MyArrayExeption.java
@@ -25,14 +25,14 @@ public class MyArrayExeption {
                 {"11", "2", "7", "8"},
                 {"7", "8", "12", "11"},
                 {"11", "2", "7", "8"}};
-        try {
+
             try {
                 Array(arr);
 
             } catch (MyArraySizeException e) {
                 System.out.println("Очень большой массив");
             }
-        }
+
         catch (MyArrayDataException e){
             System.out.println("Неправельное значение");
         }
@@ -47,7 +47,7 @@ public class MyArrayExeption {
             throw new MyArraySizeException();
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr.length != 4)
+            if (arr[i].length != 4)
                 throw new MyArraySizeException();
             for (int j = 0; j < arr.length; j++) {
                 try {
