41ac17396aff141d577104915a43c73519184054
ivan096096 <wanwanich@yandex.ru>
2022-01-24 20:34:40 +0300

    lesson-4_Chat

--- src/main/java/ru/geekbrains/lessons/lesson_3_ArrayList/HomeWork_Collection.java
+++ src/main/java/ru/geekbrains/lessons/lesson_3_ArrayList/HomeWork_Collection.java
@@ -12,80 +12,81 @@ package ru.geekbrains.lessons.lesson_3_ArrayList;
         чего нет в задании (т.е. не надо в телефонную запись добавлять еще дополнительные поля (имя, отчество, адрес),
         делать взаимодействие с пользователем через консоль и т.д.). Консоль желательно не использовать (в том числе Scanner),
         тестировать просто из метода main() прописывая add() и get().*/
+
 import java.lang.reflect.Array;
 import java.util.*;
+
 public class HomeWork_Collection {
 
 
+  public static void main(String[] args) {
 
-        public static void main(String[] args) {
+    WordsCollected();
 
-            WordsCollected();
-
-            telephoneDirectory();
-
-
-        }
-
-        private static void telephoneDirectory() {
-
-            Phone phone = new Phone();
-            phone.add("Ivanov", "11111");
-            phone.add("Ivanov", "2222");
-            phone.add("Сидоров", "3333");
-            phone.add("Сидоров", "4444");
-            phone.add("Коклюшкин", "444744");
-            phone.add("Коклюшкин", "55555");
-            phone.add("Веселов", "66666");
-
-            System.out.println(phone.get("Ivanov") );
-            System.out.println(phone.get("Сидоров"));
-            System.out.println(phone.get("Коклюшкин"));
-            System.out.println(phone.get("Веселов"));
-        }
+    telephoneDirectory();
 
 
+  }
 
-        static class Phone {
-            private final Map<String, List<String>> telephone = new HashMap<>();
+  private static void telephoneDirectory() {
 
-            public void add(String supername, String phone_number) {
-                List<String> phone_number_list;
-                if (telephone.containsKey(supername)){
-                    phone_number_list = telephone.get(supername);
-                    phone_number_list.add(phone_number);
-                }else {
-                    phone_number_list = new ArrayList<>();
-                    phone_number_list.add( phone_number);
-                    telephone.put(supername,phone_number_list);
-                }
+    Phone phone = new Phone();
+    phone.add("Ivanov", "11111");
+    phone.add("Ivanov", "2222");
+    phone.add("Сидоров", "3333");
+    phone.add("Сидоров", "4444");
+    phone.add("Коклюшкин", "444744");
+    phone.add("Коклюшкин", "55555");
+    phone.add("Веселов", "66666");
 
-            }
-            public List<String>get (String supername){
-                return telephone.get(supername);
-            }
-
-        }
-
-        private static void WordsCollected() {
-            String[] world = {"Солнце", "Закат", "Расвет", "Дождь", "Дерево", "Травка", "Птичка",
-                    "Солнце", "Закат", "Расвет", "Дождь", "Расвет",};
-            HashMap<String,Integer> map = new HashMap();
+    System.out.println(phone.get("Ivanov"));
+    System.out.println(phone.get("Сидоров"));
+    System.out.println(phone.get("Коклюшкин"));
+    System.out.println(phone.get("Веселов"));
+  }
 
 
-            for (String s : world) {
-                if(map.containsKey(s)){
-                    map.put(s, map.get(s)+1);
-                }else {
-                    map.put(s,1);
-                }
-            }
-            System.out.println(map);
+  static class Phone {
 
-        }
+    private final Map<String, List<String>> telephone = new HashMap<>();
 
+    public void add(String supername, String phone_number) {
+      List<String> phone_number_list;
+      if (telephone.containsKey(supername)) {
+        phone_number_list = telephone.get(supername);
+        phone_number_list.add(phone_number);
+      } else {
+        phone_number_list = new ArrayList<>();
+        phone_number_list.add(phone_number);
+        telephone.put(supername, phone_number_list);
+      }
 
     }
+
+    public List<String> get(String supername) {
+      return telephone.get(supername);
+    }
+
+  }
+
+  private static void WordsCollected() {
+    String[] world = {"Солнце", "Закат", "Расвет", "Дождь", "Дерево", "Травка", "Птичка",
+        "Солнце", "Закат", "Расвет", "Дождь", "Расвет",};
+    HashMap<String, Integer> map = new HashMap();
+
+    for (String s : world) {
+      if (map.containsKey(s)) {
+        map.put(s, map.get(s) + 1);
+      } else {
+        map.put(s, 1);
+      }
+    }
+    System.out.println(map);
+
+  }
+
+
+}
 
 
 
---
+++ src/main/java/ru/geekbrains/lessons/lesson_4_javafx/Launcher.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lessons.lesson_4_javafx;
+
+public class Launcher {
+
+        public static void main(String[] args) {
+            ExampleApplication.main(args);
+        }
+    }
+
